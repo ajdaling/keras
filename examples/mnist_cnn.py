ab1c120e00db3f3455f580dea0c17cc8b08ef55f
Alec <ajdaling@gmail.com>
2016-10-07 00:14:35 -0400

    added line to mnist_cnn to save weights

--- examples/mnist_cnn.py
+++ examples/mnist_cnn.py
@@ -80,3 +80,5 @@ model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
 score = model.evaluate(X_test, Y_test, verbose=0)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
+
+model.save('mnist_model2.h5')
